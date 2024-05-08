aaf492cc84ecc33fe7fd032c61af2692292df50d
Sebastián B. López <zupremacia@gmail.com>
2024-05-07 23:38:58 -0500

    PokeApp

--- app.js
+++ app.js
@@ -3,26 +3,46 @@ const namePoke2 = document.querySelector('#nombrePoke-rival');
 const poke2Tipo = document.querySelector('#tipoRival');
 const poke2Ataque = document.querySelector('#ataqueRival');
 
+const imgPoke = document.querySelector('#poke');
+const namePoke = document.querySelector('#nombrePoke-propio');
+const pokeTipo = document.querySelector('#tipoPropio');
+const pokeAtaque = document.querySelector("#ataquePropio");
+
+const input = document.querySelector('#input');
+const btnElegir = document.querySelector('#btn-poke');
+
+const btnCombate = document.querySelector('#combate');
 
 const getNumRandom = () => {
     let min = Math.ceil(0);
     let max = Math.floor(1001);
 
     return Math.floor(Math.random() * (max - min) + min);
-  }
-
-const obtenerPokePropio = ()=>{
-
 }
 
-const obtenerPokeRival = () =>{
+const obtenerPokePropio = () => {
+    const num = input.value;
+
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${num}`).then((res) => {
+
+        return res.data
+    }).then((res) => {
+        console.log(res);
+        imgPoke.src = res.sprites.back_default;
+        pokeTipo.innerHTML = res.types[0].type.name;
+        namePoke.innerHTML = res.name;
+        pokeAtaque.innerHTML = res.stats[0].base_stat;
+    })
+}
+
+const obtenerPokeRival = () => {
 
     const numPokeRival = getNumRandom();
 
-    axios.get(`https://pokeapi.co/api/v2/pokemon/${numPokeRival}`).then((res)=>{
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${numPokeRival}`).then((res) => {
 
         return res.data
-    }).then((res)=>{
+    }).then((res) => {
         console.log(res);
         imgPoke2.src = res.sprites.front_default;
         poke2Tipo.innerHTML = res.types[0].type.name;
@@ -31,11 +51,13 @@ const obtenerPokeRival = () =>{
     })
 }
 
-const combate = ()=>{
-    
+const combate = () => {
+    const ataqueRival = parseInt(poke2Ataque.textContent);
+    const ataquePropio = parseInt(pokeAtaque.textContent);
+
+    console.log(ataquePropio);
 }
 
-window.addEventListener('load', obtenerPokeRival)
-
-
-
+window.addEventListener('load', obtenerPokeRival);
+btnElegir.addEventListener('click', obtenerPokePropio);
+btnCombate.addEventListener('click', combate);
